537d402f0e248b9e7c1bc18df353ef4e149abfe6
Evandro Pedro <evandro.epam@gmail.com>
2022-08-20 03:32:33 -0300

    questões 1 e 2
    Questões 1 e 2 finalizadas e testadas.

--- lista_de_exercicios_1/lista1/bin/lista1.dart
+++ lista_de_exercicios_1/lista1/bin/lista1.dart
@@ -1,5 +1,75 @@
 import 'package:lista1/lista1.dart' as lista1;
 
+import 'dart:convert';
+import 'dart:io';
+
+void menu() {
+  print("\x1B[2J\x1B[0;0H"); // clear entire screen, move cursor to 0;0
+  print('Menu.');
+  print('Digite o número da questão que deseja executar (de 1 a 15)');
+  print('ou digite 0 para sair:');
+}
+
 void main(List<String> arguments) {
-  print('Hello world: ${lista1.calculate()}!');
+  String entrada;
+  do {
+    menu();
+    entrada = stdin.readLineSync(encoding: utf8)!;
+    switch (entrada) {
+      case '1':
+        lista1.questao1();
+        break;
+      case '2':
+        lista1.questao2();
+        break;
+      case '3':
+        lista1.questao3();
+        break;
+      case '4':
+        lista1.questao4();
+        break;
+      case '5':
+        lista1.questao5();
+        break;
+      case '6':
+        lista1.questao6();
+        break;
+      case '7':
+        lista1.questao7();
+        break;
+      case '8':
+        lista1.questao8();
+        break;
+      case '9':
+        lista1.questao9();
+        break;
+      case '10':
+        lista1.questao10();
+        break;
+      case '11':
+        lista1.questao11();
+        break;
+      case '12':
+        lista1.questao12();
+        break;
+      case '13':
+        lista1.questao13();
+        break;
+      case '14':
+        lista1.questao14();
+        break;
+      case '15':
+        lista1.questao15();
+        break;
+      case '0':
+        print('Fim do programa.');
+        break;
+      default:
+        print('Valor inválido.');
+        break;
+    }
+    print('Digite qualquer valor para continuar...');
+    stdin.readLineSync(encoding: utf8)!; // pausa para poder ver o resultado
+  } while (entrada != '0');
+  print("\x1B[2J\x1B[0;0H"); // clear entire screen, move cursor to 0;0
 }
--- lista_de_exercicios_1/lista1/lib/lista1.dart
+++ lista_de_exercicios_1/lista1/lib/lista1.dart
@@ -1,3 +1,63 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:convert';
+import 'dart:io';
+
+void questao1() {
+  print("\x1B[2J\x1B[0;0H"); // clear entire screen, move cursor to 0;0
+  print('Questão 1:');
+  print('Informe o primeiro número:');
+  double primeiroNumero = double.parse(stdin.readLineSync(encoding: utf8)!);
+  print('Informe o segundo número:');
+  double segundoNumero = double.parse(stdin.readLineSync(encoding: utf8)!);
+
+  if (primeiroNumero > segundoNumero) {
+    print('O primeiro número é maior que o segundo número.');
+  } else if (segundoNumero > primeiroNumero) {
+    print('O segundo número é maior que o primeiro número.');
+  } else {
+    print('Os dois números são iguais.');
+  }
 }
+
+void questao2() {
+  print("\x1B[2J\x1B[0;0H"); // clear entire screen, move cursor to 0;0
+  print('Questão 2:');
+  print('Preço da maçã:');
+  print('R\$0,30 até 11 unidades.');
+  print('R\$0,25 a partir de 12 unidades.\n');
+  print('Quantas deseja comprar? (Digite 0 para cancelar a compra)');
+
+  int entrada = int.parse(stdin.readLineSync(encoding: utf8)!);
+  while (entrada < 0) {
+    // validação da entrada
+    print('Entrada inválida. Informe um valor maior ou igual a 0.');
+    entrada = int.parse(stdin.readLineSync(encoding: utf8)!);
+  }
+
+  if (entrada == 0) {
+    print('Compra cancelada.');
+    return;
+  }
+
+  double precoDaMaca = 0.3;
+  if (entrada >= 12) {
+    precoDaMaca = 0.25;
+  }
+
+  String valorFinal = (entrada * precoDaMaca).toStringAsFixed(2);
+
+  print('Para comprar $entrada maçãs você pagará R\$$valorFinal.');
+}
+
+void questao3() {}
+void questao4() {}
+void questao5() {}
+void questao6() {}
+void questao7() {}
+void questao8() {}
+void questao9() {}
+void questao10() {}
+void questao11() {}
+void questao12() {}
+void questao13() {}
+void questao14() {}
+void questao15() {}
