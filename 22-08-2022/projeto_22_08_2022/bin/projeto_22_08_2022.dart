6763b3706828fc3c623677f6426ec03368a63710
Evandro Pedro <evandro.epam@gmail.com>
2022-08-22 21:38:53 -0300

    First Commit

---
+++ 22-08-2022/projeto_22_08_2022/bin/projeto_22_08_2022.dart
@@ -0,0 +1,5 @@
+import 'package:projeto_22_08_2022/projeto_22_08_2022.dart' as projeto_22_08_2022;
+
+void main(List<String> arguments) {
+  print('Hello world: ${projeto_22_08_2022.calculate()}!');
+}
