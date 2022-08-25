84ac3cc2173c93b5c38ff715a920d1d9ff2a3bd9
Evandro Pedro <evandro.epam@gmail.com>
2022-08-25 09:21:37 -0300

    atualização 24-08-2022

--- 22-08-2022/projeto_22_08_2022/bin/projeto_22_08_2022.dart
+++ 22-08-2022/projeto_22_08_2022/bin/projeto_22_08_2022.dart
@@ -1,35 +1,56 @@
-import 'package:projeto_22_08_2022/projeto_22_08_2022.dart'
-    as projeto_22_08_2022;
+import 'package:projeto_22_08_2022/projeto_22_08_2022.dart';
 import 'dart:io';
 import 'dart:convert';
 
 void main(List<String> arguments) {
   final cursos = <String, dynamic>{};
-  final curso = <String, String>{};
+  final curso = <String, String>{'nome': '', 'professor': '', 'ch': ''};
 
   String option = "";
 
-  do{
-
-    print('Menu:');
-    print('1) Adicionar curso');
-    print('2) Remover curso');
-    print('3) Atualizar curso');
-    print('4) Mostrar curso');
-    print('5) Ordenar cursos');
-    print('0) Sair');
-    print('Digite sua opção:');
-
+  do {
+    clearScreen();
+    menu();
     option = stdin.readLineSync(encoding: utf8)!;
 
-    switch (option){
+    switch (option) {
       case '1':
-        //Adicionar
-        // Nome do curso, nome do professor, carga horária
-        print('Informe o nome do curso');
-        cursos.addEntries();
+        // Entrada de dados: Código do curso, nome do curso, nome do professor, carga horária
+        print('Informe o código do curso:');
+        String codigo = stdin.readLineSync(encoding: utf8)!;
+        print('Informe o nome do curso:');
+        String nome = stdin.readLineSync(encoding: utf8)!;
+        print('Informe o nome do professor:');
+        String professor = stdin.readLineSync(encoding: utf8)!;
+        print('Informe a carga horária do curso:');
+        String cargaHoraria = stdin.readLineSync(encoding: utf8)!;
+
+        //criar um curso
+        curso.update('nome', (_) => nome);
+        curso.update('professor', (_) => professor);
+        curso.update('ch', (_) => cargaHoraria);
+
+        // adicionar a cursos;
+        cursos.putIfAbsent(codigo, () => curso);
+
+        print('Curso adicionado com sucesso.');
         break;
       case '2':
+        if (cursos.isEmpty) {
+          print('Não há cursos a serem removidos.');
+          break;
+        }
+
+        print('Informe o código do curso a ser removido:');
+        String codigo = stdin.readLineSync(encoding: utf8)!;
+
+        final cursoRemovido = cursos.remove(codigo);
+
+        if (cursoRemovido == null) {
+          print('Este código não foi encontrado. Operação cancelada.');
+        } else {
+          print('Curso ${cursoRemovido['nome']} removido com sucesso.');
+        }
         break;
       case '3':
         break;
@@ -38,12 +59,14 @@ void main(List<String> arguments) {
       case '5':
         break;
       case '0':
+        print('Saindo da aplicação...');
         break;
       default:
         print('Opção inválida. Tente novamente.');
         break;
     }
 
-
-  } while ();
+    print('Insira qualquer valor para continuar...');
+    stdin.readLineSync()!;
+  } while (option != '0');
 }
--- 22-08-2022/projeto_22_08_2022/lib/projeto_22_08_2022.dart
+++ 22-08-2022/projeto_22_08_2022/lib/projeto_22_08_2022.dart
@@ -1,3 +1,17 @@
 // int calculate() {
 //   return 6 * 7;
 // }
+void clearScreen() {
+  print("\x1B[2J\x1B[0;0H");
+}
+
+void menu() {
+  print('Menu:');
+  print('1) Adicionar curso');
+  print('2) Remover curso');
+  print('3) Atualizar curso');
+  print('4) Mostrar curso');
+  print('5) Ordenar cursos');
+  print('0) Sair');
+  print('Digite sua opção:');
+}
