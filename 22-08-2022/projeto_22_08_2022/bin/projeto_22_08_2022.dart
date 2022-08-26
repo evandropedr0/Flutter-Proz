2353fa7e9ca8651125fd128b849d61776cf80155
Evandro Pedro <evandro.epam@gmail.com>
2022-08-26 18:44:10 -0300

    Atualizações 20-08-2022 18:44

--- 22-08-2022/projeto_22_08_2022/bin/projeto_22_08_2022.dart
+++ 22-08-2022/projeto_22_08_2022/bin/projeto_22_08_2022.dart
@@ -3,54 +3,37 @@ import 'dart:io';
 import 'dart:convert';
 
 void main(List<String> arguments) {
-  final cursos = <String, dynamic>{};
-  final curso = <String, String>{'nome': '', 'professor': '', 'ch': ''};
+  final cursos = <Map<String, String>>[];
 
   String option = "";
 
   do {
-    clearScreen();
     menu();
     option = stdin.readLineSync(encoding: utf8)!;
 
     switch (option) {
       case '1':
-        // Entrada de dados: Código do curso, nome do curso, nome do professor, carga horária
-        print('Informe o código do curso:');
-        String codigo = stdin.readLineSync(encoding: utf8)!;
-        print('Informe o nome do curso:');
-        String nome = stdin.readLineSync(encoding: utf8)!;
-        print('Informe o nome do professor:');
-        String professor = stdin.readLineSync(encoding: utf8)!;
-        print('Informe a carga horária do curso:');
-        String cargaHoraria = stdin.readLineSync(encoding: utf8)!;
-
-        //criar um curso
-        curso.update('nome', (_) => nome);
-        curso.update('professor', (_) => professor);
-        curso.update('ch', (_) => cargaHoraria);
-
-        // adicionar a cursos;
-        cursos.putIfAbsent(codigo, () => curso);
-
+        final keys = <String>['codigo', 'nome', 'professor', 'carga horária'];
+        addItem(listOfMaps: cursos, keys: keys, mapName: 'curso');
         print('Curso adicionado com sucesso.');
         break;
       case '2':
-        if (cursos.isEmpty) {
-          print('Não há cursos a serem removidos.');
-          break;
+        subMenu2();
+        String op = stdin.readLineSync(encoding: utf8)!;
+
+        switch (op) {
+          case '1':
+            break;
+          case '2':
+            break;
+          case '3':
+            break;
+          case '4':
+            break;
+          default:
+            print('Opção inválida.');
         }
-
-        print('Informe o código do curso a ser removido:');
-        String codigo = stdin.readLineSync(encoding: utf8)!;
-
-        final cursoRemovido = cursos.remove(codigo);
-
-        if (cursoRemovido == null) {
-          print('Este código não foi encontrado. Operação cancelada.');
-        } else {
-          print('Curso ${cursoRemovido['nome']} removido com sucesso.');
-        }
+        removeItem(listOfMaps: cursos, key: code, mapName: 'curso');
         break;
       case '3':
         break;
--- 22-08-2022/projeto_22_08_2022/lib/projeto_22_08_2022.dart
+++ 22-08-2022/projeto_22_08_2022/lib/projeto_22_08_2022.dart
@@ -1,11 +1,15 @@
 // int calculate() {
 //   return 6 * 7;
 // }
+import 'dart:convert';
+import 'dart:io';
+
 void clearScreen() {
   print("\x1B[2J\x1B[0;0H");
 }
 
 void menu() {
+  clearScreen();
   print('Menu:');
   print('1) Adicionar curso');
   print('2) Remover curso');
@@ -14,4 +18,77 @@ void menu() {
   print('5) Ordenar cursos');
   print('0) Sair');
   print('Digite sua opção:');
+}
+
+void subMenu2() {
+  clearScreen();
+  print('Como deseja remover?');
+  print('1) Pelo código do curso');
+  print('2) Pelo nome do curso');
+  print('3) Pelo nome do(a) professor(a)');
+  print('4) Voltar');
+  print('Digite sua opção:');
+}
+
+String camelCase({required String str}) {
+  str = str.trim();
+  str += ' ';
+  int initialIndex = 0;
+
+  final substrings = <String>[];
+  for (int i = 0; i < str.length; i++) {
+    if (str[i] == ' ') {
+      substrings.add(str.substring(initialIndex, i));
+      initialIndex = i + 1;
+      i += 2;
+    }
+  }
+  str = '';
+  str += substrings[0].toLowerCase();
+  substrings.removeAt(0);
+
+  substrings.forEach((sub) {
+    sub = sub.toLowerCase();
+    sub = sub.substring(0, 1).toUpperCase() + sub.substring(1);
+    str += sub;
+  });
+  return str;
+}
+
+void updateField({required Map<String, String> map, required String mapName, required String key}) {
+  /// Updates the value of a map whose name is mapName.
+
+  mapName = mapName.toLowerCase();
+  print('Informe o(a) $key do $mapName:');
+  key = camelCase(str: key);
+
+  String input = stdin.readLineSync(encoding: utf8)!;
+  map.update(key, (_) => input);
+}
+
+void addItem({required List<Map<String, String>> listOfMaps, required List<String> keys, required String mapName}) {
+  ///Gets a list of maps <String, String> whose name is mapName
+  ///and a list of keys <String> and adds a map with the specified keys in the list
+
+  final map = <String, String>{};
+
+  keys.forEach((key) {
+    updateField(map: map, mapName: mapName, key: key);
+  });
+}
+
+void removeItem({required List<Map<String, String>> listOfMaps, required String key, required String mapName}) {
+  if (listOfMaps.isEmpty) {
+    print('Não há cursos a serem removidos.');
+    return;
+  }
+
+  print('Informe o código do curso a ser removido:');
+  String codigo = stdin.readLineSync(encoding: utf8)!;
+
+  if (cursoRemovido == null) {
+    print('Este código não foi encontrado. Operação cancelada.');
+  } else {
+    print('Curso ${cursoRemovido['nome']} removido com sucesso.');
+  }
 }
