aee168a91392ad53f18c0a406ee6bc188d6686c1
Evandro Pedro <evandro.epam@gmail.com>
2022-08-29 02:54:11 -0300

    Exercício de listas e maps finalizado.

--- 22-08-2022/projeto_22_08_2022/bin/projeto_22_08_2022.dart
+++ 22-08-2022/projeto_22_08_2022/bin/projeto_22_08_2022.dart
@@ -13,35 +13,134 @@ void main(List<String> arguments) {
 
     switch (option) {
       case '1':
-        final keys = <String>['codigo', 'nome', 'professor', 'carga horária'];
-        addItem(listOfMaps: cursos, keys: keys, mapName: 'curso');
+        // Adicionar
+        final keys = <String>['código', 'nome', 'professor', 'carga horária'];
+        final values = getValuesFromUser(keys: keys, mapName: 'curso');
+        addItem(listOfMaps: cursos, keys: keys, values: values);
         print('Curso adicionado com sucesso.');
         break;
       case '2':
+      case '3':
+        // Buscar
+        if (cursos.isEmpty) {
+          print('Lista vazia. Voltando ao menu principal...');
+          break;
+        }
+        bool voltar = false;
         subMenu2();
         String op = stdin.readLineSync(encoding: utf8)!;
 
-        switch (op) {
-          case '1':
-            break;
+        List<Map<String, String>> foundItems = [];
+        do {
+          switch (op) {
+            case '1':
+              foundItems = searchByKey(listOfMaps: cursos, key: 'código', mapName: 'curso');
+              op = '4';
+              break;
+            case '2':
+              foundItems = searchByKey(listOfMaps: cursos, key: 'nome', mapName: 'curso');
+              op = '4';
+              break;
+            case '3':
+              foundItems = searchByKey(listOfMaps: cursos, key: 'professor', mapName: 'curso');
+              op = '4';
+              break;
+            case '4':
+              voltar = true;
+              break;
+            default:
+              print('Opção inválida. Tente novamente:');
+              op = stdin.readLineSync(encoding: utf8)!;
+              continue;
+          }
+        } while (op != '4');
+
+        if (voltar) {
+          voltar = false;
+          break;
+        }
+
+        print('Foram encontrados os seguintes cursos:');
+        showListOfMaps(listOfMaps: foundItems);
+
+        switch (option) {
           case '2':
+            // Remover
+            print('Você está prestes a REMOVER os itens mostrados acima.');
+            subMenuConfirm();
+            bool confirm = stdin.readLineSync(encoding: utf8)! == '1';
+
+            if (confirm) {
+              removeMapsFromList(listOfMaps: cursos, sublistOfMaps: foundItems);
+              print('Os cursos encontrados foram removidos da lista.');
+            } else {
+              print('Operação cancelada.');
+            }
             break;
           case '3':
+            // Atualizar
+            Map<String, String> mapa;
+            if (foundItems.length > 1) {
+              print('Informe o código do curso que deseja atualizar:');
+              String codigo = stdin.readLineSync(encoding: utf8)!;
+              mapa = getMapByValue(listOfMaps: foundItems, key: 'código', value: codigo);
+              while (mapa.isEmpty) {
+                print('Código inexistente. Tente novamente:');
+                codigo = stdin.readLineSync(encoding: utf8)!;
+                mapa = getMapByValue(listOfMaps: foundItems, key: 'código', value: codigo);
+              }
+            } else {
+              mapa = foundItems.first;
+            }
+
+            print('Para ATUALIZAR os dados deste curso, siga as instruções abaixo.');
+            updateValue(map: mapa, mapName: 'curso', key: 'nome');
+            updateValue(map: mapa, mapName: 'curso', key: 'professor');
+            updateValue(map: mapa, mapName: 'curso', key: 'cargaHorária');
+
+            print('Curso atualizado:');
+            showListOfMaps(listOfMaps: [mapa]);
+
             break;
-          case '4':
+        }
+
+        break;
+      case '4':
+        // Mostrar
+        clearScreen();
+        if (cursos.isEmpty) {
+          print('Lista vazia. Não há cursos a serem mostrados.');
+          break;
+        }
+        showListOfMaps(listOfMaps: cursos);
+        break;
+      case '5':
+        // Ordenar
+        clearScreen();
+        print('Desja mostrar os cursos em que ordem?');
+        print('1) Alfabética pelo nome do curso');
+        print('2) Aleatória');
+
+        String ordem = stdin.readLineSync(encoding: utf8)!;
+        List<Map<String, String>> copiaCursos = List.from(cursos);
+
+        switch (ordem) {
+          case '1':
+            copiaCursos.sort((a, b) =>
+                a['nome']!.toUpperCase().compareTo(b['nome']!.toUpperCase()));
+            
+            showListOfMaps(listOfMaps: copiaCursos);
+            break;
+          case '2':
+            copiaCursos.shuffle();
+            showListOfMaps(listOfMaps: copiaCursos);
             break;
           default:
             print('Opção inválida.');
         }
-        removeItem(listOfMaps: cursos, key: code, mapName: 'curso');
-        break;
-      case '3':
-        break;
-      case '4':
-        break;
-      case '5':
         break;
       case '0':
+        // Sair
         print('Saindo da aplicação...');
         break;
       default:
--- 22-08-2022/projeto_22_08_2022/lib/projeto_22_08_2022.dart
+++ 22-08-2022/projeto_22_08_2022/lib/projeto_22_08_2022.dart
@@ -11,23 +11,33 @@ void clearScreen() {
 void menu() {
   clearScreen();
   print('Menu:');
-  print('1) Adicionar curso');
-  print('2) Remover curso');
-  print('3) Atualizar curso');
-  print('4) Mostrar curso');
-  print('5) Ordenar cursos');
+  print('1) Adicionar um curso');
+  print('2) Remover cursos');
+  print('3) Atualizar um curso');
+  print('4) Mostrar todos os cursos');
+  print('5) Mostrar todos os cursos em uma ordem específica');
   print('0) Sair');
   print('Digite sua opção:');
+  stdout.write('--> ');
 }
 
 void subMenu2() {
   clearScreen();
-  print('Como deseja remover?');
+  print('Buscar curso:');
   print('1) Pelo código do curso');
   print('2) Pelo nome do curso');
   print('3) Pelo nome do(a) professor(a)');
   print('4) Voltar');
   print('Digite sua opção:');
+  stdout.write('--> ');
+}
+
+void subMenuConfirm() {
+  print('Tem certeza que deseja continuar com a operação?');
+  print('Para confirmar, digite 1.');
+  print('Para cancelar, digite qualquer outro valor.');
+  print('Digite sua opção:');
+  stdout.write('--> ');
 }
 
 String camelCase({required String str}) {
@@ -55,40 +65,126 @@ String camelCase({required String str}) {
   return str;
 }
 
-void updateField({required Map<String, String> map, required String mapName, required String key}) {
+void updateValue({required Map<String, String> map, required String mapName, required String key}) {
   /// Updates the value of a map whose name is mapName.
 
   mapName = mapName.toLowerCase();
-  print('Informe o(a) $key do $mapName:');
-  key = camelCase(str: key);
+  print('Informe o(a) $key do $mapName (deixe em branco para não alterar):');
 
   String input = stdin.readLineSync(encoding: utf8)!;
-  map.update(key, (_) => input);
+  if (input.isNotEmpty) {
+    map.update(key, (_) => input);
+  }
 }
 
-void addItem({required List<Map<String, String>> listOfMaps, required List<String> keys, required String mapName}) {
+List<String> getValuesFromUser({required List<String> keys, required String mapName}) {
+  List<String> values = [];
+
+  for (int i = 1; i < keys.length; i++) {
+    mapName = mapName.toLowerCase();
+    print('Informe o(a) ${keys[i]} do $mapName:');
+
+    String input = stdin.readLineSync(encoding: utf8)!;
+
+    values.add(input);
+  }
+
+  return values;
+}
+
+void addItem(
+    {required List<Map<String, String>> listOfMaps, required List<String> keys, required List<String> values}) {
   ///Gets a list of maps <String, String> whose name is mapName
   ///and a list of keys <String> and adds a map with the specified keys in the list
 
   final map = <String, String>{};
 
+  // A primeira chave é o código do curso e é gerada automaticamente pelo tamanho da lista.
+  // Se a lista estiver vazia, o primeiro valor será 0, se tiver mais de um elemento, recebe o valor do último mais um.
+  map.putIfAbsent(camelCase(str: keys[0]), () {
+    if (listOfMaps.isEmpty) {
+      return '0';
+    } else {
+      return (int.parse(listOfMaps[listOfMaps.length - 1].values.first) + 1).toStringAsFixed(0);
+    }
+  });
+  keys.removeAt(0);
+
   keys.forEach((key) {
-    updateField(map: map, mapName: mapName, key: key);
+    int index = keys.indexOf(key);
+    map.putIfAbsent(camelCase(str: key), () => values[index]);
+  });
+  listOfMaps.add(map);
+}
+
+List<Map<String, String>> searchByKey(
+    {required List<Map<String, String>> listOfMaps, required String key, required String mapName}) {
+  print('Informe o $key do $mapName:');
+  String input = stdin.readLineSync(encoding: utf8)!;
+
+  List<Map<String, String>> foundItems = [];
+
+  listOfMaps.forEach((map) {
+    if (map[key]!.toLowerCase() == input.toLowerCase()) {
+      foundItems.add(map);
+    }
+  });
+
+  return foundItems;
+}
+
+String undoCamelCase({required String str}) {
+  String output = '';
+  List<String> sub = [];
+  int initialIndex = 0;
+
+  String word = str + str[str.length - 1].toUpperCase();
+
+  for (int i = 0; i < word.length; i++) {
+    if (word[i] == word[i].toUpperCase()) {
+      sub.add(word.substring(initialIndex, i));
+      initialIndex = i;
+      i += 1;
+    }
+  }
+
+  for (int i = 0; i < sub.length; i++) {
+    output += sub[i][0].toUpperCase() + sub[i].substring(1).toLowerCase();
+
+    if (i != sub.length - 1) {
+      output += ' ';
+    }
+  }
+
+  return output;
+}
+
+void showListOfMaps({required List<Map<String, String>> listOfMaps}) {
+  listOfMaps.forEach((curso) {
+    curso.entries.forEach((entry) {
+      print('${undoCamelCase(str: entry.key)}: ${entry.value}');
+    });
+    print('');
   });
 }
 
-void removeItem({required List<Map<String, String>> listOfMaps, required String key, required String mapName}) {
-  if (listOfMaps.isEmpty) {
-    print('Não há cursos a serem removidos.');
-    return;
-  }
+void removeMapsFromList(
+    {required List<Map<String, String>> listOfMaps, required List<Map<String, String>> sublistOfMaps}) {
+  sublistOfMaps.forEach((item) {
+    ///Removes a sublist of maps from a list of maps
+    listOfMaps.removeWhere((map) => map == item);
+  });
+}
 
-  print('Informe o código do curso a ser removido:');
-  String codigo = stdin.readLineSync(encoding: utf8)!;
+Map<String, String> getMapByValue(
+    {required List<Map<String, String>> listOfMaps, required String key, required String value}) {
+  /// Returns the Mao<String, String> within a List that contains the value for the specified key
+  /// Example: List<Map<String, String>> champions = [{'country': 'Brazil', 'worldCupWins': '5'}, {'country': 'Italy', 'worldCupWins': '4'}]
+  /// getMapByCode(listOfMaps: champions, key: 'country', value: 'Brazil') returns {'country': 'Brazil', 'worldCupWins': '5'}
+  Map<String, String> map = {};
 
-  if (cursoRemovido == null) {
-    print('Este código não foi encontrado. Operação cancelada.');
-  } else {
-    print('Curso ${cursoRemovido['nome']} removido com sucesso.');
-  }
+  listOfMaps.forEach((m) {
+    if (m[key] == value) map = m;
+  });
+  return map;
 }
