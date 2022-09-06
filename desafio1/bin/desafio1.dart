527b0c1fafa87e6470314f1f5d3cc16b9f2b3310
Evandro Pedro <evandro.epam@gmail.com>
2022-09-06 14:10:40 -0300

    Nova pasta para desafio final do módulo 1

---
+++ desafio1/bin/desafio1.dart
@@ -0,0 +1,5 @@
+import 'package:desafio1/desafio1.dart' as desafio1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${desafio1.calculate()}!');
+}
---
+++ desafio1/test/desafio1_test.dart
@@ -0,0 +1,8 @@
+import 'package:desafio1/desafio1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
