c9bf3ca1a0a175890fce3b61c4e3ac12e20d5fb4
Evandro Pedro <evandro.epam@gmail.com>
2022-09-13 15:18:10 -0300

    Classe Address finalizada

--- desafio1/lib/address.dart
+++ desafio1/lib/address.dart
@@ -1,47 +1,17 @@
 class Address {
-  String _publicPlace = '';
-  String _number = '';
-  String _complement = '';
-  String _neighbourhood = '';
-  String _state = '';
-  String _zipCode = '';
+  String publicPlace;
+  String number;
+  String complement;
+  String neighbourhood;
+  String state;
+  String zipCode;
 
   Address({
-    required String publicPlace,
-    required String number,
-    required String complement,
-    required String neighbourhood,
-    required String state,
-    required String zipCode,
-  }) {
-    _publicPlace = publicPlace;
-    _number = number;
-    _complement = complement;
-    _neighbourhood = neighbourhood;
-    _state = state;
-    _zipCode = zipCode;
-  }
-
-  String get publicPlace => _publicPlace;
-  String get number => _publicPlace;
-  String get complement => _publicPlace;
-  String get neighbourhood => _publicPlace;
-  String get state => _publicPlace;
-  String get zipCode => _publicPlace;
-
-  set publicPlace(String publicPlace) {
-    if (publicPlace.isNotEmpty) {
-      _publicPlace = publicPlace;
-    } else {
-      print('Invalid public place. It cannot be empty.');
-    }
-  }
-
-  set number(String number) {
-    if (number.isNotEmpty) {
-      _number = number;
-    } else {
-      print('Invalid number. It cannot be empty.');
-    }
-  }
+    required this.publicPlace,
+    required this.number,
+    required this.complement,
+    required this.neighbourhood,
+    required this.state,
+    required this.zipCode,
+  });
 }
