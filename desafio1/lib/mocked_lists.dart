7c66aa4f84eee25f25af9c5ddbda83eb7017e68c
Evandro Pedro <evandro.epam@gmail.com>
2022-09-18 22:41:28 -0300

    Atualização de dados e criação de funções populateList e onlyNumbers

--- desafio1/lib/mocked_lists.dart
+++ desafio1/lib/mocked_lists.dart
@@ -1,40 +1,44 @@
+import 'package:desafio1/address.dart';
+import 'package:desafio1/company.dart';
+import 'package:desafio1/individual_person.dart';
+
 class MockedLists {
   static const List<Map> mockedCompanies = [
     {
-      "fantasyName": "Julia e Vicente Telas ME",
-      "corporateName": "Julia e Vicente Telas",
+      "fantasyName": "Zara e Vicente Telas",
+      "corporateName": "Zara e Vicente Telas ME",
       "cnpj": "05.971.187/0001-94",
       "phone": "(11) 2563-5439",
       "street": "Rua Nilo Meireles",
       "number": "355",
-      "neighboorhood": "Santa Inês",
+      "neighborhood": "Santa Inês",
       "city": "Rio Branco",
       "state": "AC",
       "zipCode": "72907-184",
       "complement": "",
     },
     {
-      "fantasyName": "Isabelle e Yuri Alimentos ME",
-      "corporateName": "Isabelle e Yuri Alimentos",
+      "fantasyName": "Isabelle e Yuri Alimentos",
+      "corporateName": "Isabelle e Yuri Alimentos ME",
       "cnpj": "82.231.861/0001-45",
       "phone": "(19) 3573-4507",
       "street": "Rua Tabelião Luiz Paiva e Silva",
       "number": "100",
-      "neighboorhood": "Areias",
+      "neighborhood": "Areias",
       "city": "Teresina",
       "state": "PI",
       "zipCode": "58027-847",
       "complement": "",
     },
     {
-      "fantasyName": "Louise e Sara Entulhos Ltda",
-      "corporateName": "Louise e Sara Entulhos",
+      "fantasyName": "Louise e Sara Entulhos",
+      "corporateName": "Louise e Sara Entulhos Ltda.",
       "cnpj": "97.989.597/0001-80",
       "phone": "(11) 3635-6427",
       "cep": "76901-133",
       "street": "Rua Acre",
       "number": "603",
-      "neighboorhood": "Jardim Presidencial",
+      "neighborhood": "Jardim Presidencial",
       "city": "Ji-Paraná",
       "state": "RO",
       "zipCode": "39400-367",
@@ -43,7 +47,7 @@ class MockedLists {
   ];
   static const List<Map> mockedAddresses = [
     {
-      "neighboorhood": 'Vila Lacerda',
+      "neighborhood": 'Vila Lacerda',
       "zipCode": '13214-060',
       "complement": 'Casa',
       "state": 'SP',
@@ -52,7 +56,7 @@ class MockedLists {
       "city": 'Jundiaí',
     },
     {
-      "neighboorhood": 'Traviú',
+      "neighborhood": 'Traviú',
       "zipCode": '13213-265',
       "complement": 'Casa',
       "state": 'SP',
@@ -61,7 +65,7 @@ class MockedLists {
       "city": 'Jundiaí',
     },
     {
-      "neighboorhood": 'Jardim Estância Brasil',
+      "neighborhood": 'Jardim Estância Brasil',
       "zipCode": '12949-098',
       "complement": 'Casa',
       "state": 'SP',
@@ -70,11 +74,11 @@ class MockedLists {
       "city": 'Atibaia',
     }
   ];
-  static const List<Map> mockedPhysicalPeople = [
+  static const List<Map> mockedIndividualPeople = [
     {
       "name": "Geraldo Gustavo Samuel Drumond",
       "cpf": "435.298.711-50",
-      "neighboorhood": 'Vila Lacerda',
+      "neighborhood": 'Vila Lacerda',
       "zipCode": '13214-060',
       "complement": 'Casa',
       "state": 'SP',
@@ -85,7 +89,7 @@ class MockedLists {
     {
       "name": "Stefany Nina Aparecida da Cruz",
       "cpf": "880.952.450-03",
-      "neighboorhood": 'Traviú',
+      "neighborhood": 'Traviú',
       "zipCode": '13213-265',
       "complement": 'Casa',
       "state": 'SP',
@@ -96,7 +100,7 @@ class MockedLists {
     {
       "name": "Rosângela Alana da Luz",
       "cpf": "981.360.370-43",
-      "neighboorhood": 'Jardim Estância Brasil',
+      "neighborhood": 'Jardim Estância Brasil',
       "zipCode": '12949-098',
       "complement": 'Casa',
       "state": 'SP',
@@ -114,11 +118,56 @@ class MockedLists {
       "street": "Rua Nilo Meireles",
       "number": "596",
       "phone": "(11) 3527-6058",
-      "neighboorhood": "Santa Inês",
+      "neighborhood": "Santa Inês",
       "city": "Rio Branco",
       "state": "AC",
       "zipCode": "72907-184",
       "complement": "",
     },
   ];
+
+  static String onlyNumbers(String input) {
+    String output = '';
+    for (int i = 0; i < input.length; i++) {
+      if (int.tryParse(input[i]) != null) {
+        output += input[i];
+      }
+    }
+    return output;
+  }
+
+  static void populateList(List<Company> companies) {
+    int i = 0;
+    for (var company in mockedCompanies) {
+      companies.add(Company(
+        corporateName: company['corporateName'],
+        fantasyName: company['fantasyName'],
+        cnpj: onlyNumbers(company['cnpj']),
+        address: Address(
+          publicPlace: company['street'],
+          number: company['number'],
+          complement: company['complement'],
+          neighborhood: company['neighborhood'],
+          city: company['city'],
+          state: company['state'],
+          zipCode: onlyNumbers(company['zipCode']),
+        ),
+        associate: IndividualPerson(
+          name: mockedIndividualPeople[i]['name'],
+          cpf: onlyNumbers(mockedIndividualPeople[i]['cpf']),
+          address: Address(
+            publicPlace: mockedIndividualPeople[i]['street'],
+            number: mockedIndividualPeople[i]['number'],
+            complement: mockedIndividualPeople[i]['complement'],
+            neighborhood: mockedIndividualPeople[i]['neighborhood'],
+            city: mockedIndividualPeople[i]['city'],
+            state: mockedIndividualPeople[i]['state'],
+            zipCode: onlyNumbers(mockedIndividualPeople[i]['zipCode']),
+          ),
+        ),
+        phone: onlyNumbers(company['phone']),
+      ));
+      i++;
+    }
+  }
 }
